05ad62880f7ee82f225753ffa1edec3ef8c6dc8a
michael-hawker <michael.hawker@outlook.com>
2018-11-06 17:36:00 -0800

    Swap Pivot for WCT TabView and back with ItemsSource

---
+++ TabViewTear/Models/DataItem.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+
+namespace TabViewTear.Models
+{
+    public class DataItem: DependencyObject
+    {
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Title.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(DataItem), new PropertyMetadata(string.Empty));
+
+        public string Content
+        {
+            get { return (string)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Content.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register("Content", typeof(string), typeof(DataItem), new PropertyMetadata(string.Empty));
+    }
+}
--- TabViewTear/Views/MainPage.xaml.cs
+++ TabViewTear/Views/MainPage.xaml.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-
+using TabViewTear.Models;
 using Windows.UI.Xaml.Controls;
 
 namespace TabViewTear.Views
 {
     public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
+        ObservableCollection<DataItem> TabItems = new ObservableCollection<DataItem>();
+
         public MainPage()
         {
             InitializeComponent();
+
+            TabItems.Add(new DataItem()
+            {
+                Title = "Item 1",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a consectetur arcu, eu imperdiet nisl. Nunc id interdum odio. Aliquam non vulputate sem. Proin lacinia, lacus vitae finibus malesuada, leo libero interdum nisl, et dictum justo tortor semper tortor. Phasellus suscipit malesuada ultrices. Cras sodales vel lectus quis mattis. Sed consequat mollis ultrices. Nam eleifend purus sit amet massa mattis facilisis. Donec fringilla convallis nibh eget venenatis. Morbi ac venenatis ex. Integer ultrices velit eget dictum ultrices. Nunc aliquet lectus vitae feugiat varius. Nulla erat nisi, scelerisque ut sollicitudin id, vestibulum at mi. Donec neque velit, ornare consectetur aliquet id, egestas nec sapien. Nulla nec magna sed nunc varius bibendum."
+            });
+            TabItems.Add(new DataItem()
+            {
+                Title = "Item 2",
+                Content = "Aliquam fringilla euismod neque sit amet porta. Aliquam et ligula in neque ullamcorper interdum sit amet et magna. Quisque maximus accumsan lorem at rhoncus. Pellentesque mattis, eros non accumsan auctor, libero turpis sodales urna, id porta mi dolor at elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec lacinia leo arcu, vitae malesuada sapien consequat eget. Pellentesque vestibulum interdum convallis. Mauris nulla elit, tempus sit amet enim finibus, suscipit tempor ante. Nullam pulvinar libero sed tincidunt sagittis. Suspendisse potenti. Nulla porta lacinia lacus vel bibendum. Sed sagittis dignissim leo, ac gravida sem mattis pellentesque."
+            });
+            TabItems.Add(new DataItem()
+            {
+                Title = "Item 3",
+                Content = "Donec tellus nisl, volutpat vel urna eu, vestibulum sollicitudin sapien. Aliquam libero ex, egestas ut dapibus ullamcorper, mattis non nisl. Pellentesque quis hendrerit nibh. In lobortis placerat interdum. Aliquam et eleifend velit. Nunc ipsum orci, auctor eget eros non, euismod accumsan quam. Nam sit amet convallis est. Integer eget mauris pharetra, fringilla elit a, eleifend felis. Nullam vel ex posuere, blandit tellus nec, lobortis mauris. Nulla rhoncus nisi vel leo condimentum, non cursus lacus tempus. "
+            });
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
